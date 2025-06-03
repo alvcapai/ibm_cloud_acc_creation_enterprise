26ed922986fa67e15236f3234da0050b57237984
Álvaro Capai <Alvaro.Capai@ibm.com>
2025-06-03 13:20:16 -0300

    fix

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ resource "ibm_enterprise_account" "enterprise_account" {
   parent = var.parent
   name = var.account_name
   owner_iam_id = var.owner_iam_id
-  traits {
-    mfa = "NONE"
-    enterprise_iam_managed = true
-  }
-  options {
-    create_iam_service_id_with_apikey_and_owner_policies = true
-  }
-}
+#   traits {
+#     mfa = "NONE"
+#     enterprise_iam_managed = true
+   }
+#   options {
+#     create_iam_service_id_with_apikey_and_owner_policies = true
+#   }
+#}
